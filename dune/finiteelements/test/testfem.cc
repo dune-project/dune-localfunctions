48c1447d09de37a3c4eb932efa13e138cfb58dec
Peter Bastian <peter@dune-project.org>
2008-11-03 12:21:53 +0000

    test verzeichnis
    [[Imported from SVN: r23]]

---
+++ dune/finiteelements/test/testfem.cc
@@ -0,0 +1,13 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+#include <iostream>
+
+
+int main(int argc, char** argv)
+{
+  std::cout << "bla" << std::endl;
+  return 0;
+}

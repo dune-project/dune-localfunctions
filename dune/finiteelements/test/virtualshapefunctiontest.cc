e03735a31fe5ddaede8487155e5d9b15e6d5bee9
Oliver Sander <sander@dune-project.org>
2009-02-24 11:06:27 +0000

    Test whether local basis sets sum up to 1.  Also test the new RefinedP1Basis
    [[Imported from SVN: r78]]

--- dune/finiteelements/test/virtualshapefunctiontest.cc
+++ dune/finiteelements/test/virtualshapefunctiontest.cc
@@ -9,6 +9,7 @@
 #include <dune/finiteelements/pk2d.hh>
 #include <dune/finiteelements/q12d.hh>
 
+#include <dune/finiteelements/refinedp1.hh>
 
 /** \file
     \brief Performs some tests for the Lagrange shape functions
@@ -28,6 +29,33 @@ typedef C0LocalBasisTraits<D,2,FieldVector<D,2>,R,1,FieldVector<R,1> > C0Traits;
 typedef C1LocalBasisTraits<D,2,FieldVector<D,2>,R,1,FieldVector<R,1>,
     FieldVector<FieldVector<R,2>,1> > C1Traits;
 
+
+/** \brief Test whether all basis functions in the set sum up to one. */
+template <int dim>
+void testSumToOne(const C0LocalBasisInterface<C1Traits>* localBasis,
+                  const GeometryType& type)
+{
+  const QuadratureRule<double,dim> quad = QuadratureRules<double,dim>::rule(type,5);
+
+  for (size_t i=0; i<quad.size(); i++) {
+
+    std::vector<FieldVector<double,1> > values;
+    localBasis->evaluateFunction(quad[i].position(), values);
+
+    R sum = R(0);
+    for (int j=0; j<values.size(); j++)
+      sum += values[j];
+
+    if (std::abs(1-sum) > epsilon) {
+      std::cerr << "Shapefunction bug: shape functions do not sum up to 1 at " << quad[i].position() << std::endl;
+      success = false;
+    }
+
+  }
+
+}
+
+/** \brief Test derivative implementations using a finite difference approximation */
 template <int dim>
 void testShapeFunctionSet(const C1LocalBasisInterface<C1Traits>* localBasis,
                           const GeometryType& type)
@@ -40,7 +68,7 @@ void testShapeFunctionSet(const C1LocalBasisInterface<C1Traits>* localBasis,
 
   // A set of test points
   // Bad: dependence on dune-grid.  Only for the test points, though
-  const QuadratureRule<double,dim> quad = QuadratureRules<double,dim>::rule(type,localBasis->order());
+  const QuadratureRule<double,dim> quad = QuadratureRules<double,dim>::rule(type,5);
 
   for (size_t i=0; i<quad.size(); i++) {
 
@@ -95,16 +123,24 @@ int main (int argc, char *argv[]) try
 {
 
   Pk2DLocalFiniteElement<double,double,1> testSetk1;
+  testSumToOne<2>(&testSetk1.localBasis(), testSetk1.type());
   testShapeFunctionSet<2>(&testSetk1.localBasis(), testSetk1.type());
 
   Pk2DLocalFiniteElement<double,double,2> testSetk2;
+  testSumToOne<2>(&testSetk2.localBasis(), testSetk2.type());
   testShapeFunctionSet<2>(&testSetk2.localBasis(), testSetk2.type());
 
   Pk2DLocalFiniteElement<double,double,3> testSetk3;
+  testSumToOne<2>(&testSetk3.localBasis(), testSetk3.type());
   testShapeFunctionSet<2>(&testSetk3.localBasis(), testSetk3.type());
 
   Q12DLocalFiniteElement<double,double> testSetQ1;
+  testSumToOne<2>(&testSetQ1.localBasis(), testSetQ1.type());
   testShapeFunctionSet<2>(&testSetQ1.localBasis(), testSetQ1.type());
+
+  RefinedP1LocalFiniteElement<double,double> testSetRefinedP1;
+  testSumToOne<2>(&testSetRefinedP1.localBasis(), testSetRefinedP1.type());
+  testShapeFunctionSet<2>(&testSetRefinedP1.localBasis(), testSetRefinedP1.type());
 
   return success ? 0 : 1;
 }

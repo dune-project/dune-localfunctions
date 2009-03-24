373d5a9552828594ea43e6411c56572e5acb77ac
Stefan Lang <stefan@dune-project.org>
2009-03-24 16:01:39 +0000

    , added for template parameter
    [[Imported from SVN: r93]]

--- dune/finiteelements/p12d.hh
+++ dune/finiteelements/p12d.hh
@@ -20,7 +20,7 @@ namespace Dune
                                      LocalFiniteElementTraits<P12DLocalBasis<D,R>,P12DLocalCoefficients,
                                          P12DLocalInterpolation<P12DLocalBasis<D,R> > >
 #ifndef DUNE_VIRTUAL_SHAPEFUNCTIONS
-                                     P12DLocalFiniteElement<D,R>
+                                     , P12DLocalFiniteElement<D,R>
 #endif
                                      >
   {

3150c0265d2fb49d340cb045cd004a6aae100d17
Jorrit Fahlke <joe@dune-project.org>
2009-06-17 18:24:53 +0000

    First (unchecked) version of the global interface to the 3D edge Elements
    [[Imported from SVN: r137]]

--- dune/finiteelements/edges03d/edges03dlocalbasis.hh
+++ dune/finiteelements/edges03d/edges03dlocalbasis.hh
@@ -43,7 +43,8 @@ namespace Dune
     //! contruct a local basis instance with default orientations
     EdgeS03DLocalBasis()
     {
-      s[0] = 1; s[1] = 1; s[2] = sr2; s[3] = 1; s[4] = sr2; s[5] = sr2;
+      for(int i = 0; i < 6; ++i)
+        s[i] = 1;
     }
 
     //! contruct a local basis instance with the given orientations
@@ -52,8 +53,9 @@ namespace Dune
     //! 0 = 1 means inverted orientation for the first shape function.
     EdgeS03DLocalBasis(unsigned int orientations)
     {
-      s[0] = 1; s[1] = 1; s[2] = sr2; s[3] = 1; s[4] = sr2; s[5] = sr2;
-      for(int i = 0; i < 3; ++i)
+      for(int i = 0; i < 6; ++i)
+        s[i] = 1;
+      for(int i = 0; i < 6; ++i)
         if(orientations & (1<<i)) s[i] *= -1;
     }
 
@@ -252,6 +254,111 @@ namespace Dune
       out[5][2] = s[5]*(        in[1]      );
     }
 
+    //! Get global values of shape functions
+    /**
+     * Experimental interface for getting global values of shape functions.
+     *
+     * <b>WARNING: this global interface will result in scaled shape functions
+     * compared to the shapefunctions from the local interface with the
+     * apropriate transformation applied.  Don't use both interfaces in the
+     * same code.</b>
+     *
+     * Vector valued shape functions often require a complicated
+     * transformation to get the global values from the local values:
+     * \f[ \psi_i(g(\mathbf{\hat x}))=L(\hat\psi_i(\mathbf x)) \f]
+     * where \f$g\f$ is transformation for coordinates, \f$L\f$ the
+     * tranformation for values and the hat \f$\hat{\phantom x}\f$ denotes
+     * local quantities.  Worse, if we require for edge elements that
+     * \f[ \psi_i\cdot\mathbf t_j=\delta_{ij}\text{ on edge }j \f]
+     * and
+     * \f[ \hat\psi_i\cdot\mathbf{\hat t}_j=\delta_{ij}\text{ on edge }j \f]
+     * then we need a different \f$L\f$ for each shape function.  This
+     * property is very desirable be eases debugging and the construction of
+     * constraints for grids with hanging nodes.
+     *
+     * \tparam     Geometry Type of geometry
+     * \param[in]  in       Where to evaluate.  <b>NOTE: these are local
+     *                      coordinates</b>
+     * \param[out] out      The global values of the shape functions.
+     * \param[in]  geometry The geometry used for the local to global mapping.
+     */
+    template<typename Geometry>
+    inline void evaluateFunctionGlobal
+      (const typename Traits::DomainType& in,
+      std::vector<typename Traits::RangeType>& out,
+      const Geometry &geometry) const
+    {
+      typename Traits::DomainType pos = geometry.global(in);
+      // store coefficients
+      FieldVector<typename Traits::DomainFieldType, 6> coeff[6];
+
+      coefficientsGlobal(coeff, geometry);
+
+      out.resize(6);
+      for(int i = 0; i < 6; ++i) {
+        // \phi^i
+        out[i][0] = s[i]*( coeff[i][A01]*pos[1]+coeff[i][A02]*pos[2]+coeff[i][a0]);
+        out[i][0] = s[i]*(-coeff[i][A01]*pos[0]+coeff[i][A12]*pos[2]+coeff[i][a1]);
+        out[i][0] = s[i]*(-coeff[i][A02]*pos[0]-coeff[i][A12]*pos[1]+coeff[i][a2]);
+      }
+    }
+
+    //! Evaluate global Jacobian of all shape functions
+    /**
+     * Experimental interface for getting global values of the Jacobian of the
+     * shapefunctions.
+     *
+     * <b>WARNING: this global interface will result in scaled shape functions
+     * compared to the shapefunctions from the local interface with the
+     * apropriate transformation applied.  Don't use both interfaces in the
+     * same code.</b>
+     *
+     * This calculates the global Jacobian
+     * \f[
+     *   \mathrm J(\psi^i)=
+     *   \begin{pmatrix}
+     *     \partial\psi^i_0/\partial x_0 & \partial\psi^i_0/\partial x_1 & \cdots \\
+     *     \partial\psi^i_1/\partial x_0 & \partial\psi^i_1/\partial x_1 & \cdots \\
+     *     \vdots & \vdots & \ddots
+     *   \end{pmatrix}
+     * \f]
+     * Note that this are the derivatives of the global values by the global
+     * coordinates, evaluated at local coordinates.
+     *
+     * \tparam     Geometry Type of geometry
+     * \param[in]  in       Where to evaluate.  <b>NOTE: these are local
+     *                      coordinates</b>
+     * \param[out] out      The values of the global Jacobian.
+     * \param[in]  geometry The geometry used for the local to global mapping.
+     */
+    template<typename Geometry>
+    inline void evaluateJacobianGlobal
+      (const typename Traits::DomainType& in,         // position
+      std::vector<typename Traits::JacobianType>& out,        // return value
+      const Geometry &geometry) const
+    {
+      // store coefficients
+      FieldVector<typename Traits::DomainFieldType, 6> coeff[6];
+
+      coefficientsGlobal(coeff, geometry);
+
+      out.resize(6);
+      for(int i = 0; i < 3; ++i) {
+        // \phi^i
+        out[i][0][0] =  0;
+        out[i][0][1] =  s[i]*coeff[i][A01];
+        out[i][0][2] =  s[i]*coeff[i][A02];
+
+        out[i][1][0] = -s[i]*coeff[i][A01];
+        out[i][1][1] =  0;
+        out[i][1][2] =  s[i]*coeff[i][A12];
+
+        out[i][2][0] = -s[i]*coeff[i][A02];
+        out[i][2][1] = -s[i]*coeff[i][A12];
+        out[i][2][2] =  0;
+      }
+    }
+
     //! \brief Evaluate Jacobian of all shape functions
     inline void
     evaluateJacobian (const typename Traits::DomainType& in,         // position
@@ -291,12 +398,52 @@ namespace Dune
     }
 
   private:
-    //! square root of 2
-    static const R sr2;
+    template<typename Geometry>
+    void coefficientsGlobal
+      (FieldVector<typename Traits::DomainFieldType, 6> (&coeff)[6],
+      const Geometry &geometry) const
+    {
+      static const GenericReferenceElement<typename Traits::DomainFieldType, 3> &refElem
+        = GenericReferenceElements<typename Traits::DomainFieldType, 3>::simplex();
+
+      assert(geometry.type().isTetrahedron());
+
+      typename Traits::DomainType vertex[4];
+      for(int i = 0; i < 4; ++i)
+        vertex[i] = geometry.corner(i);
+
+      // assemble matrix and distance vectors
+      FieldMatrix<typename Traits::DomainFieldType, 6, 6> M;
+      typename Traits::DomainType distance[6];
+      for(int j = 0; j < 6; ++j) {
+        int v0 = refElem.subEntity(j, 2, 0, 3);
+        int v1 = refElem.subEntity(j, 2, 1, 3);
+        if(v0 > v1) std::swap(v0, v1);
+
+        distance[j] = vertex[v1]; distance[j] -= vertex[v0];
+
+        M[j][0] = vertex[v1][0]*vertex[v0][1] - vertex[v1][1]*vertex[v0][0];
+        M[j][1] = vertex[v1][0]*vertex[v0][2] - vertex[v1][2]*vertex[v0][0];
+        M[j][2] = vertex[v1][1]*vertex[v0][2] - vertex[v1][2]*vertex[v0][1];
+        M[j][3] = distance[j][0];
+        M[j][4] = distance[j][1];
+        M[j][5] = distance[j][2];
+      }
+
+      M.invert();
+
+      for(int i = 0; i < 6; ++i) {
+        FieldVector<typename Traits::DomainFieldType, 3> rhs(0);
+        rhs[i] = distance[i].two_norm();
+
+        M.mv(rhs, coeff[0]);
+      }
+    }
+
+    // indices into the coefficient vectors for clearer code
+    enum { A01, A02, A12, a0, a1, a2 };
 
     R s[6];
   };
-  template<class D, class R>
-  const R EdgeS03DLocalBasis<D,R>::sr2 = std::sqrt(R(2.0));
 }
 #endif // DUNE_EDGES03DLOCALBASIS_HH

dac2662653bc72b4f65340b5735e5136b51c9ef3
Jorrit Fahlke <joe@dune-project.org>
2009-06-18 10:06:08 +0000

    document the localinterpolation interface a little
    [[Imported from SVN: r139]]

--- dune/finiteelements/common/localinterpolation.hh
+++ dune/finiteelements/common/localinterpolation.hh
@@ -3,18 +3,28 @@
 #ifndef DUNE_LOCALINTERPOLATION_HH
 #define DUNE_LOCALINTERPOLATION_HH
 
-#include <iostream>
 #include <vector>
 
 namespace Dune
 {
 
+  //! Interface class for interpolating a local basis
   template<class Imp>
   class LocalInterpolationInterface
   {
   public:
 
     //! determine coefficients interpolating a given function
+    /**
+     * \tparam F Type of function to interpolate.  The class should provide a
+     *           method <tt>void evaluate(const DomainType &x, RangeType &y)
+     *           const</tt> which is used to evaluate the function on the
+     *           reference element.
+     * \tparam C Type of coefficients vector.
+     *
+     * \param[in]  f   Function instance used to interpolate.
+     * \param[out] out Resulting coefficients vector.
+     */
     template<typename F, typename C>
     void interpolate (const F& f, std::vector<C>& out) const
     {

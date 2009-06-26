3707173f801cd3e15d3405aa428fa4a8a2a15d02
Carsten Gräser <graeser@dune-project.org>
2009-06-26 13:55:29 +0000

    Added test for consistency of localInterpolation
    [[Imported from SVN: r151]]

--- dune/finiteelements/test/testfem.cc
+++ dune/finiteelements/test/testfem.cc
@@ -4,6 +4,7 @@
 #include "config.h"
 #endif
 #include <iostream>
+#include <typeinfo>
 #include <vector>
 
 #include "../p0.hh"
@@ -17,6 +18,10 @@
 #include "../q13d.hh"
 #include "../q22d.hh"
 #include "../rt02d.hh"
+#include "../refinedp1.hh"
+
+double epsilon = 1e-14;
+double sqrt_epsilon = std::sqrt(epsilon);
 
 class Func
 {
@@ -29,6 +34,84 @@ public:
     y[0] = exp(-3.0*c.two_norm2());
   }
 };
+
+template<class FE>
+class LocalFEFunction
+{
+public:
+  typedef typename FE::Traits::LocalBasisType::Traits::RangeFieldType CT;
+
+  LocalFEFunction(const FE fe) :
+    fe_(fe)
+  {
+    resetCoefficients();
+  }
+
+  void resetCoefficients()
+  {
+    coeff_.resize(fe_.localBasis().size());
+    for(int i=0; i<coeff_.size(); ++i)
+      coeff_[i] = 0;
+  }
+
+
+  template<typename DT, typename RT>
+  void evaluate (const DT& x, RT& y) const
+  {
+    typedef typename FE::Traits::LocalBasisType::Traits::RangeType RT;
+
+    std::vector<RT> yy;
+    fe_.localBasis().evaluateFunction(x, yy);
+
+    y = 0.0;
+    for (int i=0; i<yy.size(); ++i)
+      y.axpy(coeff_[i], yy[i]);
+  }
+
+  std::vector<CT> coeff_;
+
+private:
+  const FE fe_;
+};
+
+
+template<class FE>
+bool testLocalInterpolation(const FE& fe)
+{
+  bool success = true;
+  LocalFEFunction<FE> f(fe);
+
+  std::vector<typename LocalFEFunction<FE>::CT> coeff;
+  for(int i=0; i<f.coeff_.size(); ++i)
+  {
+    f.coeff_[i] = 1;
+    fe.localInterpolation().interpolate(f, coeff);
+    if (coeff.size() != fe.localBasis().size())
+    {
+      std::cout << "Bug in LocalInterpolation for finite element type "
+                << typeid(FE).name() << std::endl;
+      std::cout << "    Interpolation vector has size " << coeff.size() << std::endl;
+      std::cout << "    Basis has size " << fe.localBasis().size() << std::endl;
+      success = false;
+    }
+
+
+    double diff=0;
+    for(int j=0; j<coeff.size(); ++j)
+    {
+      if (std::abs(coeff[i]-f.coeff_[i]) > sqrt_epsilon)
+      {
+        std::cout << "Bug in LocalInterpolation for finite element type "
+                  << typeid(FE).name() << std::endl;
+        std::cout << "    Interpolation weight " << j
+                  << " differs significantly from coefficient of linear combination." << std::endl;
+        success = false;
+      }
+    }
+  }
+  return success;
+}
+
 
 int main(int argc, char** argv)
 {
@@ -43,6 +126,7 @@ int main(int argc, char** argv)
   Dune::Q12DLocalFiniteElement<double,double> q13dlfem;
   Dune::Q22DLocalFiniteElement<double,double> q22dlfem;
   Dune::RT02DLocalFiniteElement<double,double> rt02dlfem;
+  Dune::RefinedP1LocalFiniteElement<double,double> refp1lfem;
 
   std::vector<double> c;
 
@@ -56,6 +140,20 @@ int main(int argc, char** argv)
   q12dlfem.localInterpolation().interpolate(Func(),c);
   q13dlfem.localInterpolation().interpolate(Func(),c);
   q22dlfem.localInterpolation().interpolate(Func(),c);
+  refp1lfem.localInterpolation().interpolate(Func(),c);
 
-  return 0;
+  bool success = true;
+
+  success = (testLocalInterpolation(p0lfem) and success);
+  success = (testLocalInterpolation(p1lfem) and success);
+  success = (testLocalInterpolation(p11dlfem) and success);
+  success = (testLocalInterpolation(p12dlfem) and success);
+  success = (testLocalInterpolation(p13dlfem) and success);
+  success = (testLocalInterpolation(pk2dlfem) and success);
+  success = (testLocalInterpolation(q1lfem) and success);
+  success = (testLocalInterpolation(q12dlfem) and success);
+  success = (testLocalInterpolation(q13dlfem) and success);
+  success = (testLocalInterpolation(refp1lfem) and success);
+
+  return success;
 }

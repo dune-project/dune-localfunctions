6297c11ecc8d51e08dd31f7866d95ec680998ef9
Oliver Sander <sander@dune-project.org>
2009-09-02 07:56:09 +0000

    implement interpolation for 1d, 2d, 3d
    [[Imported from SVN: r196]]

--- dune/finiteelements/hierarchicalp2.hh
+++ dune/finiteelements/hierarchicalp2.hh
@@ -6,13 +6,12 @@
 #include <dune/common/geometrytype.hh>
 
 #include "common/localfiniteelement.hh"
-#include "hierarchicalp2/hierarchicalp2/p2localbasis.hh"
+
+#include "hierarchicalp2/hierarchicalsimplexp2localbasis.hh"
+#include "hierarchicalp2/hierarchicalsimplexp2localinterpolation.hh"
+
 #include "pk2d/pk2dlocalcoefficients.hh"
-#include "pk2d/pk2dlocalinterpolation.hh"
-#include "pk2d/pk2dlocalbasis.hh"
 #include "pk3d/pk3dlocalcoefficients.hh"
-#include "pk3d/pk3dlocalinterpolation.hh"
-#include "pk3d/pk3dlocalbasis.hh"
 
 namespace Dune
 {
@@ -33,9 +32,9 @@ namespace Dune
    */
   template<class D, class R>
   class HierarchicalP2LocalFiniteElement<D,R,2> : LocalFiniteElementInterface<
-                                                      LocalFiniteElementTraits<HierarchicalP2LocalBasis<D,R,2>,
+                                                      LocalFiniteElementTraits<HierarchicalSimplexP2LocalBasis<D,R,2>,
                                                           Pk2DLocalCoefficients<2>,
-                                                          Pk2DLocalInterpolation<Pk2DLocalBasis<D,R,2> > >
+                                                          HierarchicalSimplexP2LocalInterpolation<HierarchicalSimplexP2LocalBasis<D,R,2> > >
 #ifndef DUNE_VIRTUAL_SHAPEFUNCTIONS
                                                       , HierarchicalP2LocalFiniteElement<D,R,2>
 #endif
@@ -44,9 +43,9 @@ namespace Dune
   public:
     /** \todo Please doc me !
      */
-    typedef LocalFiniteElementTraits<HierarchicalP2LocalBasis<D,R,2>,
+    typedef LocalFiniteElementTraits<HierarchicalSimplexP2LocalBasis<D,R,2>,
         Pk2DLocalCoefficients<2>,
-        Pk2DLocalInterpolation<Pk2DLocalBasis<D,R,2> > > Traits;
+        HierarchicalSimplexP2LocalInterpolation<HierarchicalSimplexP2LocalBasis<D,R,2> > > Traits;
 
     /** \todo Please doc me !
      */
@@ -84,9 +83,9 @@ namespace Dune
     }
 
   private:
-    HierarchicalP2LocalBasis<D,R,2> basis;
+    HierarchicalSimplexP2LocalBasis<D,R,2> basis;
     Pk2DLocalCoefficients<2> coefficients;
-    Pk2DLocalInterpolation<Pk2DLocalBasis<D,R,2> > interpolation;
+    HierarchicalSimplexP2LocalInterpolation<HierarchicalSimplexP2LocalBasis<D,R,2> > interpolation;
     GeometryType gt;
   };
 
@@ -94,9 +93,9 @@ namespace Dune
    */
   template<class D, class R>
   class HierarchicalP2LocalFiniteElement<D,R,3> : LocalFiniteElementInterface<
-                                                      LocalFiniteElementTraits<HierarchicalP2LocalBasis<D,R,3>,
+                                                      LocalFiniteElementTraits<HierarchicalSimplexP2LocalBasis<D,R,3>,
                                                           Pk3DLocalCoefficients<2>,
-                                                          Pk3DLocalInterpolation<Pk3DLocalBasis<D,R,2> > >
+                                                          HierarchicalSimplexP2LocalInterpolation<HierarchicalSimplexP2LocalBasis<D,R,3> > >
 #ifndef DUNE_VIRTUAL_SHAPEFUNCTIONS
                                                       , HierarchicalP2LocalFiniteElement<D,R,3>
 #endif
@@ -105,9 +104,9 @@ namespace Dune
   public:
     /** \todo Please doc me !
      */
-    typedef LocalFiniteElementTraits<HierarchicalP2LocalBasis<D,R,3>,
+    typedef LocalFiniteElementTraits<HierarchicalSimplexP2LocalBasis<D,R,3>,
         Pk3DLocalCoefficients<2>,
-        Pk3DLocalInterpolation<Pk3DLocalBasis<D,R,2> > > Traits;
+        HierarchicalSimplexP2LocalInterpolation<HierarchicalSimplexP2LocalBasis<D,R,3> > > Traits;
 
     /** \todo Please doc me !
      */
@@ -145,9 +144,9 @@ namespace Dune
     }
 
   private:
-    HierarchicalP2LocalBasis<D,R,3> basis;
+    HierarchicalSimplexP2LocalBasis<D,R,3> basis;
     Pk3DLocalCoefficients<2> coefficients;
-    Pk3DLocalInterpolation<Pk3DLocalBasis<D,R,2> > interpolation;
+    HierarchicalSimplexP2LocalInterpolation<HierarchicalSimplexP2LocalBasis<D,R,3> > interpolation;
     GeometryType gt;
   };
 
---
+++ dune/finiteelements/hierarchicalp2/hierarchicalsimplexp2localinterpolation.hh
@@ -0,0 +1,104 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_HIERARCHICAL_SIMPLEX_P2_LOCALINTERPOLATION_HH
+#define DUNE_HIERARCHICAL_SIMPLEX_P2_LOCALINTERPOLATION_HH
+
+#include "../common/localinterpolation.hh"
+
+namespace Dune
+{
+  /**
+     \tparam LB The LocalBasis implementation
+   */
+  template<class LB>
+  class HierarchicalSimplexP2LocalInterpolation
+    : public LocalInterpolationInterface<HierarchicalSimplexP2LocalInterpolation<LB> >
+  {
+  public:
+
+    template<typename F, typename C>
+    void interpolate (const F& f, std::vector<C>& out) const
+    {
+      typename LB::Traits::DomainType x;
+      typename LB::Traits::RangeType y;
+
+      dune_static_assert(LB::Traits::dimDomain <=3, "LocalInterpolation for HierarchicalSimplexP2 finite elements"
+                         " is only implemented for dimDomain <=3!");
+
+      switch (LB::Traits::dimDomain) {
+
+      case 1 :
+
+        out.resize(3);
+
+        // First: the two vertex dofs
+        x[0] = 0.0;   f.evaluate(x, y);    out[0] = y;
+        x[0] = 1.0;   f.evaluate(x, y);    out[2] = y;
+
+        // Then: the edge dof
+        x[0] = 0.5;   f.evaluate(x, y);
+        out[1] = y - 0.5*(out[0] + out[2]);
+
+        break;
+
+
+      case 2 :
+
+        out.resize(6);
+
+        // First: the three vertex dofs
+        x[0] = 0.0;    x[1] = 0.0;      f.evaluate(x, y);    out[0] = y;
+        x[0] = 1.0;    x[1] = 0.0;      f.evaluate(x, y);    out[2] = y;
+        x[0] = 0.0;    x[1] = 1.0;      f.evaluate(x, y);    out[5] = y;
+
+        // Then: the three edge dofs
+        x[0] = 0.5;    x[1] = 0.0;      f.evaluate(x, y);
+        out[1] = y - 0.5*(out[0] + out[2]);
+
+        x[0] = 0.0;    x[1] = 0.5;      f.evaluate(x, y);
+        out[3] = y - 0.5*(out[0] + out[5]);
+
+        x[0] = 0.5;    x[1] = 0.5;      f.evaluate(x, y);
+        out[4] = y - 0.5*(out[2] + out[5]);
+
+        break;
+
+      case 3 :
+
+        out.resize(10);
+
+        // First: the four vertex dofs
+        x[0] = 0.0;    x[1] = 0.0;     x[2] = 0.0;    f.evaluate(x, y);    out[0] = y;
+        x[0] = 1.0;    x[1] = 0.0;     x[2] = 0.0;    f.evaluate(x, y);    out[2] = y;
+        x[0] = 0.0;    x[1] = 1.0;     x[2] = 0.0;    f.evaluate(x, y);    out[5] = y;
+        x[0] = 0.0;    x[1] = 0.0;     x[2] = 1.0;    f.evaluate(x, y);    out[9] = y;
+
+        // Then: the six edge dofs
+        x[0] = 0.5;    x[1] = 0.0;     x[2] = 0.0;    f.evaluate(x, y);
+        out[1] = y - 0.5*(out[0] + out[2]);
+
+        x[0] = 0.0;    x[1] = 0.5;     x[2] = 0.0;    f.evaluate(x, y);
+        out[3] = y - 0.5*(out[0] + out[5]);
+
+        x[0] = 0.5;    x[1] = 0.5;     x[2] = 0.0;    f.evaluate(x, y);
+        out[4] = y - 0.5*(out[2] + out[5]);
+
+        x[0] = 0.0;    x[1] = 0.0;     x[2] = 0.5;    f.evaluate(x, y);
+        out[6] = y - 0.5*(out[0] + out[9]);
+
+        x[0] = 0.5;    x[1] = 0.0;     x[2] = 0.5;    f.evaluate(x, y);
+        out[7] = y - 0.5*(out[2] + out[9]);
+
+        x[0] = 0.0;    x[1] = 0.5;     x[2] = 0.5;    f.evaluate(x, y);
+        out[8] = y - 0.5*(out[5] + out[9]);
+
+        break;
+
+      }
+
+    }
+
+  };
+}
+
+#endif

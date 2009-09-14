5c2f4fdd36f649355125f812d081ef7fae1002aa
Peter Bastian <peter@dune-project.org>
2009-09-14 15:10:21 +0000

    RT0 for hexahedra
    [[Imported from SVN: r207]]

---
+++ dune/finiteelements/rt0q3d.hh
@@ -0,0 +1,63 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_RT0Q3DLOCALFINITEELEMENT_HH
+#define DUNE_RT0Q3DLOCALFINITEELEMENT_HH
+
+#include <dune/common/geometrytype.hh>
+
+#include "common/localfiniteelement.hh"
+#include "rt0q3d/rt0q3dall.hh"
+
+namespace Dune
+{
+  template<class D, class R>
+  class RT0Q3DLocalFiniteElement : LocalFiniteElementInterface<
+                                       LocalFiniteElementTraits<RT0Q3DLocalBasis<D,R>,RT0Q3DLocalCoefficients,
+                                           RT0Q3DLocalInterpolation<RT0Q3DLocalBasis<D,R> > >
+#ifndef DUNE_VIRTUAL_SHAPEFUNCTIONS
+                                       , RT0Q3DLocalFiniteElement<D,R>
+#endif
+                                       >
+  {
+  public:
+    typedef LocalFiniteElementTraits<RT0Q3DLocalBasis<D,R>,RT0Q3DLocalCoefficients,
+        RT0Q3DLocalInterpolation<RT0Q3DLocalBasis<D,R> > > Traits;
+
+    RT0Q3DLocalFiniteElement ()
+    {
+      gt.makeHexahedron();
+    }
+
+    RT0Q3DLocalFiniteElement (int s) : basis(s), interpolation(s)
+    {
+      gt.makeHexahedron();
+    }
+
+    const typename Traits::LocalBasisType& localBasis () const
+    {
+      return basis;
+    }
+
+    const typename Traits::LocalCoefficientsType& localCoefficients () const
+    {
+      return coefficients;
+    }
+
+    const typename Traits::LocalInterpolationType& localInterpolation () const
+    {
+      return interpolation;
+    }
+
+    GeometryType type () const
+    {
+      return gt;
+    }
+
+  private:
+    RT0Q3DLocalBasis<D,R> basis;
+    RT0Q3DLocalCoefficients coefficients;
+    RT0Q3DLocalInterpolation<RT0Q3DLocalBasis<D,R> > interpolation;
+    GeometryType gt;
+  };
+}
+#endif

234c779c11c37ca9aec2aa267a2460332364ea88
Martin Nolte <mnolte@dune-project.org>
2009-10-05 15:24:04 +0000

    original log: nolte (r191) beginnings of a Lagrange interpolation (which will give rise to the Lagrange basis)
    [[Imported from SVN: r282]]

---
+++ dune/finiteelements/generic/dune/finiteelements/lagrangeinterpolation.hh
@@ -0,0 +1,45 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_LAGRANGEINTERPOLATION_HH
+#define DUNE_LAGRANGEINTERPOLATION_HH
+
+#include <dune/finiteelements/lagrangepoints.hh>
+
+namespace Dune
+{
+
+  template< class Topology, class F >
+  class LocalLagrangeInterpolation
+  {
+    typedef LocalLagrangeInterpolation< Topology, F > This;
+
+  public:
+    typedef F Field;
+
+    static const unsigned int dimDomain = Topology::dimension;
+
+  private:
+    LagrangePoints< Topology, F > lagrangePoints_;
+
+  public:
+    LocalLagrangeInterpolation ( const unsigned int order )
+      : lagrangePoints_( order )
+    {}
+
+    template< class Function >
+    void interpolate ( const Function &function, std::vector< Field > &coefficients )
+    {
+      typedef typename LagrangePoints< Topology, F >::iterator Iterator;
+
+      coefficients.resize( lagrangePoints_.size() );
+
+      unsigned int index = 0;
+      const Iterator end = lagrangePoints_.end();
+      for( Iterator it = lagrangePoints_.begin(); it != end; ++it )
+        coefficients_[ index++ ] = function( *it );
+    }
+  };
+
+}
+
+#endif

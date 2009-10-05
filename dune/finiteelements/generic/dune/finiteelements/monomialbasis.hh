d060bce65faefc124d2463a587c9750da11baf0f
Martin Nolte <mnolte@dune-project.org>
2009-10-05 15:23:19 +0000

    original log: nolte (r157) started types for MonomialBasis
    [[Imported from SVN: r261]]

--- dune/finiteelements/generic/dune/finiteelements/monomialbasis.hh
+++ dune/finiteelements/generic/dune/finiteelements/monomialbasis.hh
@@ -6,9 +6,30 @@
 namespace Dune
 {
 
-  template< class Topology, unsigned int order >
-  class MonomialBasis
-  {};
+  // MonomialBasis
+  // -------------
+
+  template< class Topology, class F, unsigned int k >
+  class MonomialBasis;
+
+  template< class F, unsigned int k >
+  class MonomialBasis< GenericGeometry::Point, F, k >
+  {
+    typedef MonomialBasis< GenericGeometry::Point, F, k > This;
+
+  public:
+    typedef GenericGeometry::Point Topology;
+    typedef F Field;
+
+    static const unsigned int dimDomain = Topology::dimension;
+    static const unsigned int dimRange = 1;
+
+    typedef FieldVector< Field, dimDomain > DomainVector;
+    typedef FieldVector< Field, dimRange > RangeVector;
+
+    static const unsigned int order = k;
+    static const unsigned int numBasisFunctions = 1;
+  };
 
 }
 

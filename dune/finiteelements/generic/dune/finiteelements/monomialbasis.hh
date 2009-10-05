f65444679f3fb5ad3d48fec801892ba38ed48d6c
Martin Nolte <mnolte@dune-project.org>
2009-10-05 15:23:17 +0000

    original log: nolte (r156) added new directory for dune-localfunctions specific stuff
    [[Imported from SVN: r260]]

---
+++ dune/finiteelements/generic/dune/finiteelements/monomialbasis.hh
@@ -0,0 +1,15 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_MONOMIALBASIS_HH
+#define DUNE_MONOMIALBASIS_HH
+
+namespace Dune
+{
+
+  template< class Topology, unsigned int order >
+  class MonomialBasis
+  {};
+
+}
+
+#endif

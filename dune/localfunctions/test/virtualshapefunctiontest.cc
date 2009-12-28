cd6af20a2e76376e3fe3a3ac7f94e62a095b47c7
Oliver Sander <sander@dune-project.org>
2009-12-28 15:34:14 +0000

    Test the LocalCoefficients class through the virtual interface
    [[Imported from SVN: r652]]

--- dune/localfunctions/test/virtualshapefunctiontest.cc
+++ dune/localfunctions/test/virtualshapefunctiontest.cc
@@ -15,11 +15,47 @@
 
 using namespace Dune;
 
+void testLocalCoefficients(const LocalCoefficientsVirtualInterface* localCoefficients)
+{
+  if (!localCoefficients)
+    DUNE_THROW(Dune::Exception, "Received an invalid pointer to LocalCoefficientsVirtualInterface");
+
+  if (localCoefficients->size() < 1)
+    DUNE_THROW(Dune::Exception, "LocalCoefficients does not provide any coefficients!");
+
+  for (std::size_t i=0; i<localCoefficients->size(); i++) {
+
+    // Test the localKey method
+    // We just test whether the interface is there.  Correctness is tested elsewhere
+    unsigned int subEntity = localCoefficients->localKey(i).subEntity();
+    unsigned int codim     = localCoefficients->localKey(i).codim();
+    unsigned int index     = localCoefficients->localKey(i).index();
+
+  }
+}
+
+// Test all methods of a local finite element given as a pointer to the abstract base class
+template <class LocalBasisTraits>
+void testLocalFiniteElement(const LocalFiniteElementVirtualInterface<LocalBasisTraits>* localFiniteElement)
+{
+  // Test method type()
+  std::cout << "Testing local finite element for a " << localFiniteElement->type() << "." << std::endl;
+
+  // Test the local basis
+
+  // Test the local coefficients
+  testLocalCoefficients(&localFiniteElement->localCoefficients());
+
+  // Test the interpolation
+}
+
 
 int main (int argc, char *argv[]) try
 {
 
-  const Dune::LocalFiniteElementVirtualImp<Dune::P1LocalFiniteElement<double, double, 2> > virtualLocalSimplexFE_;
+  const Dune::LocalFiniteElementVirtualImp<Dune::P1LocalFiniteElement<double, double, 2> > virtualLocalSimplexFE;
+
+  testLocalFiniteElement(&virtualLocalSimplexFE);
 
   return 0;
 

4ec2fbf05720e976261125e3c4c5af87b94a53b0
Oliver Sander <sander@dune-project.org>
2010-01-15 17:31:28 +0000

    Backward compatibility headers.  These will be remove _before_ the next release!
    [[Imported from SVN: r695]]

---
+++ dune/localfunctions/rt0q.hh
@@ -0,0 +1,10 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_RT0QLOCALFINITEELEMENT_HH
+#define DUNE_RT0QLOCALFINITEELEMENT_HH
+
+#warning This header is deprecated!  Please use raviartthomas/raviartthomas0q.hh instead.
+
+#include "raviartthomas/raviartthomas0q.hh"
+
+#endif
---
+++ dune/localfunctions/rt0q3d.hh
@@ -0,0 +1,10 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_RT0Q3DLOCALFINITEELEMENT_HH
+#define DUNE_RT0Q3DLOCALFINITEELEMENT_HH
+
+#warning This header is deprecated!  Please use raviartthomas/raviartthomas0q3d.hh instead.
+
+#include "raviartthomas/raviartthomas0q3d.hh"
+
+#endif

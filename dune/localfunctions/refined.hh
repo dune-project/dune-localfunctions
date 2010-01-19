1ec4db5cf0eae3ed7fc8b731227c40bb7593af20
Oliver Sander <sander@dune-project.org>
2010-01-19 12:06:27 +0000

    add convenience header that includes all elements based on uniform refinement
    [[Imported from SVN: r742]]

---
+++ dune/localfunctions/refined.hh
@@ -0,0 +1,10 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+/** \file
+    \brief Convenience header that includes all available LocalFiniteElements
+    based on uniform element refinement.
+ */
+
+
+#include <dune/localfunctions/refined/refinedp0.hh>
+#include <dune/localfunctions/refined/refinedp1.hh>

e2c45c2424ea2f9038cb0b17b6a4ffe47d6ba8dc
Oliver Sander <sander@dune-project.org>
2010-01-20 16:18:34 +0000

    backward compatibility headers
    [[Imported from SVN: r751]]

---
+++ dune/localfunctions/p1.hh
@@ -0,0 +1,10 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_P1_LOCALFINITEELEMENT_DEPRECATED_HH
+#define DUNE_P1_LOCALFINITEELEMENT_DEPRECATED_HH
+
+#warning This header is deprecated!  Please use lagrange/p1.hh or lagrange.hh instead.
+
+#include "lagrange/p1.hh"
+
+#endif
---
+++ dune/localfunctions/q1.hh
@@ -0,0 +1,10 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_Q0_LOCALFINITEELEMENT_DEPRECATED_HH
+#define DUNE_Q0_LOCALFINITEELEMENT_DEPRECATED_HH
+
+#warning This header is deprecated!  Please use lagrange/q0.hh or lagrange.hh instead.
+
+#include "lagrange/q0.hh"
+
+#endif

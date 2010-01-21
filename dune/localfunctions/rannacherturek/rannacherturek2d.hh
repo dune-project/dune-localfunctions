5013fb878348bf44ce0d6be0a5f84bb5eb0d9dd8
Oliver Sander <sander@dune-project.org>
2010-01-21 17:23:23 +0000

    adjust path
    [[Imported from SVN: r774]]

--- dune/localfunctions/rannacherturek/rannacherturek2d.hh
+++ dune/localfunctions/rannacherturek/rannacherturek2d.hh
@@ -7,9 +7,9 @@
 
 #include <dune/localfunctions/common/localfiniteelementtraits.hh>
 
-#include "rannacher_turek2d/rannacher_turek2dlocalbasis.hh"
-#include "rannacher_turek2d/rannacher_turek2dlocalcoefficients.hh"
-#include "rannacher_turek2d/rannacher_turek2dlocalinterpolation.hh"
+#include "rannacherturek2d/rannacher_turek2dlocalbasis.hh"
+#include "rannacherturek2d/rannacher_turek2dlocalcoefficients.hh"
+#include "rannacherturek2d/rannacher_turek2dlocalinterpolation.hh"
 
 namespace Dune {
 

21c203858f9c610a4d362aa8633e0bb8f53b8364
Oliver Sander <sander@dune-project.org>
2010-01-21 17:28:20 +0000

    convenience header for the Rannacher-Turek elements
    [[Imported from SVN: r777]]

---
+++ dune/localfunctions/rannacherturek.hh
@@ -0,0 +1,8 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+/** \file
+    \brief Convenience header that includes all available Rannacher-Turek LocalFiniteElements
+ */
+
+
+#include <dune/localfunctions/rannacherturek/rannacherturek2d.hh>

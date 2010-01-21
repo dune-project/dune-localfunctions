fe8b8827149a71bb0f932d8c766400cb63fd530f
Oliver Sander <sander@dune-project.org>
2010-01-21 10:07:50 +0000

    fix copy'n'paste bug
    [[Imported from SVN: r761]]

--- dune/localfunctions/q1.hh
+++ dune/localfunctions/q1.hh
@@ -1,10 +1,10 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
-#ifndef DUNE_Q0_LOCALFINITEELEMENT_DEPRECATED_HH
-#define DUNE_Q0_LOCALFINITEELEMENT_DEPRECATED_HH
+#ifndef DUNE_Q1_LOCALFINITEELEMENT_DEPRECATED_HH
+#define DUNE_Q1_LOCALFINITEELEMENT_DEPRECATED_HH
 
-#warning This header is deprecated!  Please use lagrange/q0.hh or lagrange.hh instead.
+#warning This header is deprecated!  Please use lagrange/q1.hh or lagrange.hh instead.
 
-#include "lagrange/q0.hh"
+#include "lagrange/q1.hh"
 
 #endif

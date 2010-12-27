68cc0469234ad8fee8c95eb23eeb77cfa87876f8
Jorrit Fahlke <joe@dune-project.org>
2010-12-27 01:52:59 +0000

    [GlobalValue] Test for global-valued lowest order edge elements.
    [[Imported from SVN: r935]]

---
+++ dune/localfunctions/test/test-edges0.5.cc
@@ -0,0 +1,98 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <cstddef>
+#include <iostream>
+#include <ostream>
+
+#include <dune/common/exceptions.hh>
+#include <dune/common/fvector.hh>
+#include <dune/common/geometrytype.hh>
+
+#include <dune/grid/genericgeometry/geometry.hh>
+#include <dune/grid/genericgeometry/geometrytraits.hh>
+#include <dune/grid/utility/vertexorder.hh>
+
+#include <dune/localfunctions/whitney/edges0.5.hh>
+
+#include "test-fe.hh"
+
+int main(int argc, char** argv) {
+  try {
+    int result = 77;
+
+    // tolerance for floating-point comparisons
+    static const double eps = 1e-9;
+    // stepsize for numerical differentiation
+    static const double delta = 1e-5;
+
+    { // 2D
+      std::cout << "== Checking global-valued EdgeS0_5 elements (with dim=2)"
+                << std::endl;
+
+      Dune::GeometryType gt;
+      gt.makeTriangle();
+
+      Dune::FieldVector<double, 2> corners[3];
+      corners[0][0] = -.5; corners[0][1] = -.5;
+      corners[1][0] =  .5; corners[1][1] = -.5;
+      corners[2][0] = 0  ; corners[2][1] =  .5;
+      typedef Dune::GenericGeometry::BasicGeometry<
+          2, Dune::GenericGeometry::DefaultGeometryTraits<double, 2, 2>
+          > Geometry;
+      Geometry geo(gt, corners);
+
+      std::size_t vertexIds[] = {0, 1, 2};
+      Dune::GeneralVertexOrder<2, std::size_t>
+      vo(gt, vertexIds+0, vertexIds+3);
+
+      Dune::EdgeS0_5FiniteElementFactory<Geometry, double> feFactory;
+      bool success = testFE(geo, feFactory.make(geo, vo), eps, delta);
+
+      if(success && result != 1)
+        result = 0;
+      else
+        result = 1;
+    }
+
+    { // 3D
+      std::cout << "== Checking global-valued EdgeS0_5 elements (with dim=3)"
+                << std::endl;
+
+      Dune::GeometryType gt;
+      gt.makeTetrahedron();
+
+      Dune::FieldVector<double, 3> corners[4];
+      corners[0][0] = -.5; corners[0][1] = -.5; corners[0][2] = -.5;
+      corners[1][0] =  .5; corners[1][1] = -.5; corners[1][2] = -.5;
+      corners[2][0] = 0  ; corners[2][1] =  .5; corners[2][2] = -.5;
+      corners[3][0] = 0  ; corners[3][1] =  0 ; corners[3][2] =  .5;
+      typedef Dune::GenericGeometry::BasicGeometry<
+          3, Dune::GenericGeometry::DefaultGeometryTraits<double, 3, 3>
+          > Geometry;
+      Geometry geo(gt, corners);
+
+      std::size_t vertexIds[] = {0, 1, 2, 3};
+      Dune::GeneralVertexOrder<3, std::size_t>
+      vo(gt, vertexIds+0, vertexIds+4);
+
+      Dune::EdgeS0_5FiniteElementFactory<Geometry, double> feFactory;
+      bool success = testFE(geo, feFactory.make(geo, vo), eps, delta);
+
+      if(success && result != 1)
+        result = 0;
+      else
+        result = 1;
+    }
+
+    return result;
+  }
+  catch (const Dune::Exception& e) {
+    std::cout << e << std::endl;
+    throw;
+  }
+}
---
+++ dune/localfunctions/test/test-fe.hh
@@ -0,0 +1,257 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#ifndef DUNE_LOCALFUNCTIONS_TEST_TEST_FE_HH
+#define DUNE_LOCALFUNCTIONS_TEST_TEST_FE_HH
+
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <ostream>
+// #include <typeinfo>
+#include <vector>
+
+#include <dune/common/classname.hh>
+#include <dune/common/fmatrix.hh>
+
+#include <dune/grid/common/quadraturerules.hh>
+
+// This class defines a local finite element function.
+// It is determined by a local finite element and
+// representing the local basis and a coefficient vector.
+// This provides the evaluate method needed by the interpolate()
+// method.
+template<class FE>
+class FEFunction {
+  typedef typename FE::Traits::Basis::Traits::DomainLocal DomainLocal;
+  typedef typename FE::Traits::Basis::Traits::Range Range;
+
+  const FE& fe;
+
+public:
+  typedef typename FE::Traits::Basis::Traits::RangeField CT;
+
+  std::vector<CT> coeff;
+
+  FEFunction(const FE& fe_) : fe(fe_) { resetCoefficients(); }
+
+  void resetCoefficients() {
+    coeff.resize(fe.basis().size());
+    for(std::size_t i=0; i<coeff.size(); ++i)
+      coeff[i] = 0;
+  }
+
+  void setRandom(double max) {
+    coeff.resize(fe.basis().size());
+    for(std::size_t i=0; i<coeff.size(); ++i)
+      coeff[i] = ((1.0*std::rand()) / RAND_MAX - 0.5)*2.0*max;
+  }
+
+  void evaluate (const DomainLocal& x, Range& y) const {
+    std::vector<Range> yy;
+    fe.basis().evaluateFunction(x, yy);
+
+    y = 0.0;
+    for (std::size_t i=0; i<yy.size(); ++i)
+      y.axpy(coeff[i], yy[i]);
+  }
+};
+
+
+// Check if interpolation is consistent with basis evaluation.
+/**
+ * This test generates a local coefficient vector with random values from a
+ * certain range (-100..100).  It then uses the basis to wrap this coeffient
+ * vector into an element-local discrete function.  This is then interpolated
+ * into another coefficient vector using the interpolation of the finite
+ * element.  The two coefficient vectors are then compared.
+ *
+ * \param FE  The finite element to check
+ * \param eps Tolerance when comparing floating-point values
+ * \param n   Number of times to run the check.
+ */
+template<class FE>
+bool testInterpolation(const FE& fe, double eps, int n=5)
+{
+  bool success = true;
+  FEFunction<FE> f(fe);
+
+  std::vector<typename FEFunction<FE>::CT> coeff;
+  for(int i=0; i<n && success; ++i) {
+    // Set random coefficient vector
+    f.setRandom(100);
+
+    // Compute interpolation weights
+    fe.interpolation().interpolate(f, coeff);
+
+    // Check size of weight vector
+    if (coeff.size() != fe.basis().size()) {
+      std::cout << "Bug in LocalInterpolation for finite element type "
+                << Dune::className<FE>() << ":" << std::endl;
+      std::cout << "    Interpolation vector has size " << coeff.size()
+                << std::endl;
+      std::cout << "    Basis has size " << fe.basis().size() << std::endl;
+      std::cout << std::endl;
+      success = false;
+
+      // skip rest of loop since that depends on matching sizes
+      continue;
+    }
+
+    // Check if interpolation weights are equal to coefficients
+    for(std::size_t j=0; j<coeff.size() && success; ++j) {
+      if ( std::abs(coeff[j]-f.coeff[j]) >
+           eps*(std::max(std::abs(f.coeff[j]), 1.0)) )
+      {
+        std::cout << std::setprecision(16);
+        std::cout << "Bug in LocalInterpolation for finite element type "
+                  << Dune::className<FE>() << ":" << std::endl;
+        std::cout << "    Interpolation weight " << j << " differs by "
+                  << std::abs(coeff[j]-f.coeff[j]) << " from coefficient of "
+                  << "linear combination." << std::endl;
+        std::cout << std::endl;
+        success = false;
+      }
+    }
+  }
+  return success;
+}
+
+// check whether Jacobian agrees with FD approximation
+/**
+ * \param geo   The geometry the finite element is tested on.
+ * \param fe    The finite element to test.
+ * \param eps   Tolerance for comparing floating-point values.  When comparing
+ *              numerical derivatives, this is divided by \c delta to yield an
+ *              even bigger tolerance.
+ * \param delta Stepsize to use when doing numerical derivatives.
+ * \param order The Jacobian is checked at a number of quadrature points.
+ *              This parameter determines the order of the quatrature rule
+ *              used to obtain the quadrature points.
+ */
+template<class Geo, class FE>
+bool testJacobian(const Geo &geo, const FE& fe, double eps, double delta,
+                  std::size_t order = 2)
+{
+  typedef typename FE::Traits::Basis Basis;
+
+  typedef typename Basis::Traits::DomainField DF;
+  static const std::size_t dimDLocal = Basis::Traits::dimDomainLocal;
+  typedef typename Basis::Traits::DomainLocal DomainLocal;
+  static const std::size_t dimDGlobal = Basis::Traits::dimDomainGlobal;
+
+  static const std::size_t dimR = Basis::Traits::dimRange;
+  typedef typename Basis::Traits::Range Range;
+
+  typedef typename Basis::Traits::Jacobian Jacobian;
+
+  bool success = true;
+
+  // ////////////////////////////////////////////////////////////
+  //   Check the partial derivatives by comparing them
+  //   to finite difference approximations
+  // ////////////////////////////////////////////////////////////
+
+  // A set of test points
+  const Dune::QuadratureRule<DF, dimDLocal> quad =
+    Dune::QuadratureRules<DF, dimDLocal>::rule(fe.type(),order);
+
+  // Loop over all quadrature points
+  for (std::size_t i=0; i < quad.size(); i++) {
+
+    // Get a test point
+    const DomainLocal& testPoint = quad[i].position();
+
+    // Get the shape function derivatives there
+    std::vector<Jacobian> jacobians;
+    fe.basis().evaluateJacobian(testPoint, jacobians);
+    if(jacobians.size() != fe.basis().size()) {
+      std::cout << "Bug in evaluateJacobianGlobal() for finite element type "
+                << Dune::className<FE>() << ":" << std::endl;
+      std::cout << "    Jacobian vector has size " << jacobians.size()
+                << std::endl;
+      std::cout << "    Basis has size " << fe.basis().size() << std::endl;
+      std::cout << std::endl;
+      return false;
+    }
+
+    Dune::FieldMatrix<DF, dimDLocal, dimDGlobal> geoJT =
+      geo.jacobianTransposed(testPoint);
+
+    // Loop over all shape functions in this set
+    for (std::size_t j=0; j<fe.basis().size(); ++j) {
+
+      // basis.evaluateJacobian returns global derivatives, however we can
+      // only do local derivatives, so transform the derivatives back into
+      // local coordinates
+      Dune::FieldMatrix<double, dimR, dimDLocal> localJacobian(0);
+      for(std::size_t k = 0; k < dimR; ++k)
+        for(std::size_t l = 0; l < dimDGlobal; ++l)
+          for(std::size_t m = 0; m < dimDLocal; ++m)
+            localJacobian[k][m] += jacobians[j][k][l] * geoJT[m][l];
+
+      // Loop over all local directions
+      for (std::size_t m = 0; m < dimDLocal; ++m) {
+
+        // Compute an approximation to the derivative by finite differences
+        DomainLocal upPos   = testPoint;
+        DomainLocal downPos = testPoint;
+
+        upPos[m]   += delta;
+        downPos[m] -= delta;
+
+        std::vector<Range> upValues, downValues;
+
+        fe.basis().evaluateFunction(upPos,   upValues);
+        fe.basis().evaluateFunction(downPos, downValues);
+
+        //Loop over all components
+        for(std::size_t k = 0; k < dimR; ++k) {
+
+          // The current partial derivative, just for ease of notation
+          double derivative = localJacobian[k][m];
+
+          double finiteDiff = (upValues[j][k] - downValues[j][k]) / (2*delta);
+
+          // Check
+          if ( std::abs(derivative-finiteDiff) >
+               eps/delta*(std::max(std::abs(finiteDiff), 1.0)) )
+          {
+            std::cout << std::setprecision(16);
+            std::cout << "Bug in evaluateJacobian() for finite element type "
+                      << Dune::className<FE>() << ":" << std::endl;
+            std::cout << "    Shape function derivative does not agree with "
+                      << "FD approximation" << std::endl;
+            std::cout << "    Shape function " << j << " component " << k
+                      << " at position " << testPoint << ": derivative in "
+                      << "local direction " << m << " is "
+                      << derivative << ", but " << finiteDiff << " is "
+                      << "expected." << std::endl;
+            std::cout << std::endl;
+            success = false;
+          }
+        } //Loop over all components
+      } // Loop over all local directions
+    } // Loop over all shape functions in this set
+  } // Loop over all quadrature points
+
+  return success;
+}
+
+// call tests for given finite element
+template<class Geo, class FE>
+bool testFE(const Geo &geo, const FE& fe, double eps, double delta,
+            unsigned order = 2)
+{
+  bool success = true;
+
+  success = testInterpolation(fe, eps) and success;
+  success = testJacobian(geo, fe, eps, delta, order) and success;
+
+  return success;
+}
+
+#endif // DUNE_LOCALFUNCTIONS_TEST_TEST_FE_HH

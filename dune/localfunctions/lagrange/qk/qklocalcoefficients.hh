5c01453b13a8c1e4fde26b3e94bf07127e651c10
Oliver Sander <sander@dune-project.org>
2012-12-26 13:22:32 +0000

    Compute dof index-on-subentity in a dimension-independent way.
    That is actually not even all that complicated.
    
    Now we only need to also compute the subentity number per dof for
    all dimensions, and we'll have Qk elements for all dimensions.
    
    [[Imported from SVN: r1151]]

--- dune/localfunctions/lagrange/qk/qklocalcoefficients.hh
+++ dune/localfunctions/lagrange/qk/qklocalcoefficients.hh
@@ -35,12 +35,10 @@ namespace Dune
     }
 
 
-    void setup2d(std::vector<unsigned int>& subEntity,
-                 std::vector<unsigned int>& index)
+    void setup2d(std::vector<unsigned int>& subEntity)
     {
       assert(k>0);
       unsigned lastIndex=0;
-      unsigned lastInnerFaceIndex=0;
 
       // LocalKey: entity number , entity codim, dof indices within each entity
       /* edge and vertex numbering
@@ -54,56 +52,40 @@ namespace Dune
        */
 
       // lower edge (2)
-      subEntity[lastIndex] = 0;                 // corner 0
-      index[lastIndex++] = 0;
-      for (unsigned i = 0; i < k - 1; ++i) {
-        subEntity[lastIndex] = 2;           // inner dofs of lower edge (2)
-        index[lastIndex++] = i;
-      }
+      subEntity[lastIndex++] = 0;                 // corner 0
+      for (unsigned i = 0; i < k - 1; ++i)
+        subEntity[lastIndex++] = 2;           // inner dofs of lower edge (2)
 
-      subEntity[lastIndex] = 1;                 // corner 1
-      index[lastIndex++] = 0;
+      subEntity[lastIndex++] = 1;                 // corner 1
 
       // iterate from bottom to top over inner edge dofs
       for (unsigned e = 0; e < k - 1; ++e) {
-        subEntity[lastIndex] = 0;                   // left edge (0)
-        index[lastIndex++] = e;
-        for (unsigned i = 0; i < k - 1; ++i) {
-          subEntity[lastIndex] = 0;                     // face dofs
-          index[lastIndex++] = lastInnerFaceIndex++;
-        }
-        subEntity[lastIndex] = 1;                   // right edge (1)
-        index[lastIndex++] = e;
+        subEntity[lastIndex++] = 0;                   // left edge (0)
+        for (unsigned i = 0; i < k - 1; ++i)
+          subEntity[lastIndex++] = 0;                     // face dofs
+        subEntity[lastIndex++] = 1;                   // right edge (1)
       }
 
       // upper edge (3)
-      subEntity[lastIndex] = 2;                 // corner 2
-      index[lastIndex++] = 0;
-      for (unsigned i = 0; i < k - 1; ++i) {
-        subEntity[lastIndex] = 3;                   // inner dofs of upper edge (3)
-        index[lastIndex++] = i;
-      }
+      subEntity[lastIndex++] = 2;                 // corner 2
+      for (unsigned i = 0; i < k - 1; ++i)
+        subEntity[lastIndex++] = 3;                   // inner dofs of upper edge (3)
 
-      subEntity[lastIndex] = 3;                 // corner 3
-      index[lastIndex++] = 0;
+      subEntity[lastIndex++] = 3;                 // corner 3
 
       assert((StaticPower<k+1,d>::power==lastIndex));
-      assert(((k-1)*(k-1))==lastInnerFaceIndex);
     }
 
 
-    void setup3d(std::vector<unsigned int>& subEntity,
-                 std::vector<unsigned int>& index)
+    void setup3d(std::vector<unsigned int>& subEntity)
     {
       assert(k>0);
       unsigned lastIndex=0;
-      unsigned lastInnerFaceIndex=0;
 #ifndef NDEBUG
       const unsigned numIndices = StaticPower<k+1,d>::power;
       const unsigned numFaceIndices = StaticPower<k+1,d-1>::power;
 #endif
       const unsigned numInnerEdgeDofs = k-1;
-      const unsigned numInnerFaceDofs = numInnerEdgeDofs * numInnerEdgeDofs;
 
       // LocalKey: entity number , entity codim, dof indices within each entity
       /* edge and vertex numbering
@@ -118,41 +100,26 @@ namespace Dune
 
       // bottom face (4)
       lastIndex=0;
-      lastInnerFaceIndex=0;
       // lower edge (2)
-      subEntity[lastIndex] = 0;              // corner 0
-      index[lastIndex++] = 0;
-      for (unsigned i = 0; i < numInnerEdgeDofs; ++i) {
-        subEntity[lastIndex] = 6;                // inner dofs of lower edge (2)
-        index[lastIndex++] = i;
-      }
+      subEntity[lastIndex++] = 0;              // corner 0
+      for (unsigned i = 0; i < numInnerEdgeDofs; ++i)
+        subEntity[lastIndex++] = 6;                // inner dofs of lower edge (2)
 
-      subEntity[lastIndex] = 1;              // corner 1
-      index[lastIndex++] = 0;
+      subEntity[lastIndex++] = 1;              // corner 1
 
       // iterate from bottom to top over inner edge dofs
       for (unsigned e = 0; e < numInnerEdgeDofs; ++e) {
-        subEntity[lastIndex] = 4;                // left edge (4)
-        index[lastIndex++] = e;
-        for (unsigned i = 0; i < numInnerEdgeDofs; ++i) {
-          subEntity[lastIndex] = 4;                       // inner face dofs
-          index[lastIndex++] = lastInnerFaceIndex++;
-        }
-        subEntity[lastIndex] = 5;                 // right edge (5)
-        index[lastIndex++] = e;
+        subEntity[lastIndex++] = 4;                // left edge (4)
+        for (unsigned i = 0; i < numInnerEdgeDofs; ++i)
+          subEntity[lastIndex++] = 4;                       // inner face dofs
+        subEntity[lastIndex++] = 5;                 // right edge (5)
       }
 
       // upper edge (7)
-      subEntity[lastIndex] = 2;              // corner 2
-      index[lastIndex++] = 0;
-      for (unsigned i = 0; i < k - 1; ++i) {
-        subEntity[lastIndex] = 7;                // inner dofs of upper edge (7)
-        index[lastIndex++] = i;
-      }
-      subEntity[lastIndex] = 3;                // corner 3
-      index[lastIndex++] = 0;
-
-      assert(((k-1)*(k-1))==lastInnerFaceIndex);
+      subEntity[lastIndex++] = 2;              // corner 2
+      for (unsigned i = 0; i < k - 1; ++i)
+        subEntity[lastIndex++] = 7;                // inner dofs of upper edge (7)
+      subEntity[lastIndex++] = 3;                // corner 3
 
       assert(numFaceIndices==lastIndex);       // added 1 face so far
       /////////////////////////////////////////// end bottom face (4)
@@ -160,76 +127,49 @@ namespace Dune
       ///////////////////// inner faces
       for(unsigned f = 0; f < numInnerEdgeDofs; ++f) {
 
-        lastInnerFaceIndex=0;
-
         // lower edge (connecting  edges 0 and 1)
-        subEntity[lastIndex] = 0;                // dof on edge 0
-        index[lastIndex++] = f;
-        for (unsigned i = 0; i < numInnerEdgeDofs; ++i) {
-          subEntity[lastIndex] = 2;                            // dof in front face
-          index[lastIndex++] = i+(f*numInnerEdgeDofs);
-        }
-        subEntity[lastIndex] = 1;                // dof on edge 1
-        index[lastIndex++] = f;
+        subEntity[lastIndex++] = 0;                // dof on edge 0
+        for (unsigned i = 0; i < numInnerEdgeDofs; ++i)
+          subEntity[lastIndex++] = 2;                            // dof in front face
+        subEntity[lastIndex++] = 1;                // dof on edge 1
 
         // iterate from bottom to top over inner edge dofs
         for (unsigned e = 0; e < numInnerEdgeDofs; ++e) {
-          subEntity[lastIndex] = 0;                  // on left face (0)
-          index[lastIndex++] = e+f*numInnerEdgeDofs;
-          for (unsigned i = 0; i < numInnerEdgeDofs; ++i) {
-            subEntity[lastIndex] = 0;                    // volume dofs
-            index[lastIndex++] = i + f*numInnerFaceDofs + e*numInnerEdgeDofs;
-          }
-          subEntity[lastIndex] = 1;                  // right edge (5)
-          index[lastIndex++] = e + f*numInnerEdgeDofs;
+          subEntity[lastIndex++] = 0;                  // on left face (0)
+          for (unsigned i = 0; i < numInnerEdgeDofs; ++i)
+            subEntity[lastIndex++] = 0;                    // volume dofs
+          subEntity[lastIndex++] = 1;                  // right edge (5)
         }
 
         // upper edge (connecting  edges 0 and 1)
-        subEntity[lastIndex] = 2;                // dof on edge 2
-        index[lastIndex++] = f;
-        for (unsigned i = 0; i < numInnerEdgeDofs; ++i) {
-          subEntity[lastIndex] = 3;                  // dof on rear face (3)
-          index[lastIndex++] = i + f*numInnerEdgeDofs;
-        }
-        subEntity[lastIndex] = 3;                // dof on edge 3
-        index[lastIndex++] = f;
+        subEntity[lastIndex++] = 2;                // dof on edge 2
+        for (unsigned i = 0; i < numInnerEdgeDofs; ++i)
+          subEntity[lastIndex++] = 3;                  // dof on rear face (3)
+        subEntity[lastIndex++] = 3;                // dof on edge 3
 
         assert(lastIndex==(f+1+1)*numFaceIndices);
       }
 
       ////////////////////////////////////////// top face (5)
-      lastInnerFaceIndex=0;
       // lower edge (10)
-      subEntity[lastIndex] = 4;              // corner 4
-      index[lastIndex++] = 0;
-      for (unsigned i = 0; i < k - 1; ++i) {
-        subEntity[lastIndex] = 10;                // inner dofs on lower edge (10)
-        index[lastIndex++] = i;
-      }
-      subEntity[lastIndex] = 5;              // corner 5
-      index[lastIndex++] = 0;
+      subEntity[lastIndex++] = 4;              // corner 4
+      for (unsigned i = 0; i < k - 1; ++i)
+        subEntity[lastIndex++] = 10;                // inner dofs on lower edge (10)
+      subEntity[lastIndex++] = 5;              // corner 5
 
       // iterate from bottom to top over inner edge dofs
       for (unsigned e = 0; e < k - 1; ++e) {
-        subEntity[lastIndex] = 8;                // left edge (8)
-        index[lastIndex++] = e;
-        for (unsigned i = 0; i < k - 1; ++i) {
-          subEntity[lastIndex] = 5;                  // face dofs
-          index[lastIndex++] = lastInnerFaceIndex++;
-        }
-        subEntity[lastIndex] = 9;                // right edge (9)
-        index[lastIndex++] = e;
+        subEntity[lastIndex++] = 8;                // left edge (8)
+        for (unsigned i = 0; i < k - 1; ++i)
+          subEntity[lastIndex++] = 5;                  // face dofs
+        subEntity[lastIndex++] = 9;                // right edge (9)
       }
 
       // upper edge (11)
-      subEntity[lastIndex] = 6;              // corner 6
-      index[lastIndex++] = 0;
-      for (unsigned i = 0; i < k - 1; ++i) {
-        subEntity[lastIndex] = 11;                // inner dofs of upper edge (11)
-        index[lastIndex++] = i;
-      }
-      subEntity[lastIndex] = 7;              // corner 7
-      index[lastIndex++] = 0;
+      subEntity[lastIndex++] = 6;              // corner 6
+      for (unsigned i = 0; i < k - 1; ++i)
+        subEntity[lastIndex++] = 11;                // inner dofs of upper edge (11)
+      subEntity[lastIndex++] = 7;              // corner 7
 
       assert(numIndices==lastIndex);
     }
@@ -251,24 +191,40 @@ namespace Dune
             codim[i]++;
       }
 
+      // Set up index vector (the index of the dof in the set of dofs of a given subentity)
+      // Algorithm: the 'index' has the same ordering as the dof number 'i'.
+      // To make it consecutive we interpret 'i' in the (k+1)-adic system, omit all digits
+      // that correspond to axes where the dof is on the element boundary, and transform the
+      // rest to the (k-1)-adic system.
+      std::vector<unsigned int> index(size());
+
+      for (std::size_t i=0; i<size(); i++) {
+
+        index[i] = 0;
+
+        array<unsigned int,d> mIdx = multiindex(i);
+
+        for (int j=d-1; j>=0; j--)
+          if (mIdx[j]>0 and mIdx[j]<k)
+            index[i] = (k-1)*index[i] + (mIdx[j]-1);
+
+      }
+
       // Set up entity and dof numbers for each (supported) dimension separately
       std::vector<unsigned int> subEntity(li.size());
-      std::vector<unsigned int> index(li.size());
 
       if (k==1) {
 
-        for (std::size_t i=0; i<size(); i++) {
+        for (std::size_t i=0; i<size(); i++)
           subEntity[i] = i;
-          index[i]     = 0;
-        }
 
       } else if (d==2) {
 
-        setup2d(subEntity, index);
+        setup2d(subEntity);
 
       } else if (d==3) {
 
-        setup3d(subEntity, index);
+        setup3d(subEntity);
 
       } else
         DUNE_THROW(Dune::NotImplemented, "QkLocalCoefficients for k==" << k << " and d==" << d);

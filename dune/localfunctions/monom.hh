694cf3c4ca87da0edeefb61fd6ad669a97d9ca6f
Oliver Sander <sander@igpm.rwth-aachen.de>
2014-11-15 22:24:00 +0100

    Add backward-compatibility header 'monom.hh'
    All it does is include the new header 'monomial.hh', which does what the
    old 'monom.hh' header does, only under a new name.

---
+++ dune/localfunctions/monom.hh
@@ -0,0 +1,11 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#ifndef DUNE_MONOMLOCALFINITEELEMENT_HH
+#define DUNE_MONOMLOCALFINITEELEMENT_HH
+
+#warning This file is deprecated.  Please use monomial.hh instead!
+
+#include <dune/localfunctions/monomial.hh>
+
+#endif // DUNE_MONOMLOCALFINITEELEMENT_HH

ba540ffde2a0628f0cdb25748427cd2aac7216c8
Carsten Gräser <graeser@dune-project.org>
2017-11-05 01:43:26 +0100

    Add helper functions to translate between evaluate() and operator()
    The provided functions allow to wrap functions supporting
    one interface to model the other one. Using a concept check
    this just forwards functions that already support the desired interface.
    
    This is just an implementation detail for a transition process.
    The purpose is to simply add a single line to each interpolate()
    implementation in order to support both syntaxes.

---
+++ dune/localfunctions/common/localinterpolation.hh
@@ -0,0 +1,80 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_LOCALFUNCTIONS_COMMON_LOCALINTERPOLATION_HH
+#define DUNE_LOCALFUNCTIONS_COMMON_LOCALINTERPOLATION_HH
+
+#include <functional>
+
+#include <dune/common/concept.hh>
+#include <dune/common/function.hh>
+
+
+
+namespace Dune {
+
+  namespace Impl {
+
+    // Concept for function supporting f.evaluate(Domain, Range&)
+    template<class Domain, class Range>
+    struct FunctionWithEvaluate
+    {
+      template<class F>
+      auto require(F&& f) -> decltype(
+        f.evaluate(std::declval<Domain>(), std::declval<Range&>())
+      );
+    };
+
+    // Concept for function supporting f(Domain)
+    template<class Domain>
+    struct FunctionWithCallOperator
+    {
+      template<class F>
+      auto require(F&& f) -> decltype(
+        f(std::declval<Domain>())
+      );
+    };
+
+    // Create function supporting f.evaluate(Domain, Range&)
+    // If the argument already does this, just forward it.
+    template<class Domain, class Range, class F,
+      std::enable_if_t<models<FunctionWithEvaluate<Domain, Range>, F>(), int> = 0>
+    decltype(auto) makeFunctionWithEvaluate(const F& f)
+    {
+      return f;
+    }
+
+    // Create function supporting f.evaluate(Domain, Range&)
+    // If the argument does not support this, wrap it as VirtualFunction
+    template<class Domain, class Range, class F,
+      std::enable_if_t<not models<FunctionWithEvaluate<Domain, Range>, F>(), int> = 0>
+    decltype(auto) makeFunctionWithEvaluate(const F& f)
+    {
+      return makeVirtualFunction<Domain, Range>(std::cref(f));
+    }
+
+    // Create function supporting Range = f(Domain)
+    // If the argument already does this, just forward it.
+    template<class Domain, class Range, class F,
+      std::enable_if_t<models<FunctionWithCallOperator<Domain>, F>(), int> = 0>
+    decltype(auto) makeFunctionWithCallOperator(const F& f)
+    {
+      return f;
+    }
+
+    // Create function supporting Range = f(Domain)
+    // If the argument does not support this, wrap it in a lambda
+    template<class Domain, class Range, class F,
+      std::enable_if_t<not models<FunctionWithCallOperator<Domain>, F>(), int> = 0>
+    decltype(auto) makeFunctionWithCallOperator(const F& f)
+    {
+      return [&](auto&& x) {
+        Range y;
+        f.evaluate(x,y);
+        return y;
+      };
+    }
+
+  } // namespace Impl
+
+} // namespace Dune
+#endif

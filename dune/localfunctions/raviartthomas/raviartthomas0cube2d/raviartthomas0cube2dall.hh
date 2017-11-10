91859421855034b94ff42c10721e8ebfc84f380f
Oliver Sander <oliver.sander@tu-dresden.de>
2017-11-10 16:08:38 +0100

    Modernize the Raviart-Thomas implementation a bit

--- dune/localfunctions/raviartthomas/raviartthomas0cube2d/raviartthomas0cube2dall.hh
+++ dune/localfunctions/raviartthomas/raviartthomas0cube2d/raviartthomas0cube2dall.hh
@@ -143,27 +143,24 @@ namespace Dune
     //! \brief Standard constructor
     RT0Cube2DLocalInterpolation ()
     {
-      sign0 = sign1 = sign2 = sign3 = 1.0;
+      std::fill(sign_.begin(), sign_.end, 1.0);
     }
 
     //! \brief Make set numer s, where 0<=s<8
-    RT0Cube2DLocalInterpolation (unsigned int s)
+    RT0Cube2DLocalInterpolation (std::bitset<4> s)
     {
-      sign0 = sign1 = sign2 = sign3 = 1.0;
-      if (s&1) sign0 *= -1.0;
-      if (s&2) sign1 *= -1.0;
-      if (s&4) sign2 *= -1.0;
-      if (s&8) sign3 *= -1.0;
+      for (int i=0; i<4; i++)
+        sign_[i] = s[i] ? -1.0 : 1.0;
 
-      m0[0] = 0.0; m0[1] = 0.5;
-      m1[0] = 1.0; m1[1] = 0.5;
-      m2[0] = 0.5; m2[1] = 0.0;
-      m3[0] = 0.5; m3[1] = 1.0;
+      m0 = {0.0, 0.5};
+      m1 = {1.0, 0.5};
+      m2 = {0.5, 0.0};
+      m3 = {0.5, 1.0};
 
-      n0[0] = -1.0; n0[1] =  0.0;
-      n1[0] =  1.0; n1[1] =  0.0;
-      n2[0] =  0.0; n2[1] = -1.0;
-      n3[0] =  0.0; n3[1] =  1.0;
+      n0 = {-1.0,  0.0};
+      n1 = { 1.0,  0.0};
+      n2 = { 0.0, -1.0};
+      n3 = { 0.0,  1.0};
     }
 
     template<typename F, typename C>
@@ -174,15 +171,20 @@ namespace Dune
 
       out.resize(4);
 
-      f.evaluate(m0,y); out[0] = (y[0]*n0[0]+y[1]*n0[1])*sign0;
-      f.evaluate(m1,y); out[1] = (y[0]*n1[0]+y[1]*n1[1])*sign1;
-      f.evaluate(m2,y); out[2] = (y[0]*n2[0]+y[1]*n2[1])*sign2;
-      f.evaluate(m3,y); out[3] = (y[0]*n3[0]+y[1]*n3[1])*sign3;
+      // Evaluate the normal components at the edge midpoints
+      f.evaluate(m0,y); out[0] = (y[0]*n0[0]+y[1]*n0[1])*sign_[0];
+      f.evaluate(m1,y); out[1] = (y[0]*n1[0]+y[1]*n1[1])*sign_[1];
+      f.evaluate(m2,y); out[2] = (y[0]*n2[0]+y[1]*n2[1])*sign_[2];
+      f.evaluate(m3,y); out[3] = (y[0]*n3[0]+y[1]*n3[1])*sign_[3];
     }
 
   private:
-    typename LB::Traits::RangeFieldType sign0,sign1,sign2,sign3;
+    std::array<typename LB::Traits::RangeFieldType,4> sign_;
+
+    // The four edge midpoints of the reference quadrilateral
     typename LB::Traits::DomainType m0,m1,m2,m3;
+
+    // The four edge normals of the reference quadrilateral
     typename LB::Traits::DomainType n0,n1,n2,n3;
   };
 

f78eb6e55897a11077c2d06bfd76f4dfa01e49ca
Oliver Sander <oliver.sander@tu-dresden.de>
2018-12-16 06:39:08 +0100

    Canonicalize single-inclusion macros

--- dune/localfunctions/lagrange/pqkfactory.hh
+++ dune/localfunctions/lagrange/pqkfactory.hh
@@ -1,7 +1,7 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
-#ifndef DUNE_PQK_FACTORY_HH
-#define DUNE_PQK_FACTORY_HH
+#ifndef DUNE_LOCALFUNCTIONS_LAGRANGE_PQKFACTORY_HH
+#define DUNE_LOCALFUNCTIONS_LAGRANGE_PQKFACTORY_HH
 
 #include <map>
 

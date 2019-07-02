dc19d5a0b7247169ac5c95aa9b112215659d1b3d
Oliver Sander <oliver.sander@tu-dresden.de>
2019-07-02 11:50:16 +0200

    Deprecate method topologyId from class GenericLocalFiniteElement
    Because
    a) It is not a LocalFiniteElement interface method
    b) You can always get the topology id by calling type().id().

--- dune/localfunctions/utility/localfiniteelement.hh
+++ dune/localfunctions/utility/localfiniteelement.hh
@@ -95,7 +95,9 @@ namespace Dune
     }
 
     /** \todo Please doc me !
+     * \deprecated Deprecated in dune-localfunctions 2.7.  Use type().id() instead!
      */
+    DUNE_DEPRECATED_MSG("Use type().id() instead!")
     unsigned int topologyId () const
     {
       return topologyId_;

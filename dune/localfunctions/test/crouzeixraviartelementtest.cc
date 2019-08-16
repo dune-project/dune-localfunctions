3514f267b7fb171ac66baafd35fddfc6b1f9f10a
Oliver Sander <oliver.sander@tu-dresden.de>
2019-08-16 09:36:35 +0200

    Add a Crouzeix-Raviart implementation with compile-time dimension
    The implementation is generic, and the patch adds a unit test
    that tests all dimensions <= 4.

---
+++ dune/localfunctions/test/crouzeixraviartelementtest.cc
@@ -0,0 +1,26 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#include "config.h"
+
+#include <dune/localfunctions/crouzeixraviart.hh>
+
+#include <dune/localfunctions/test/test-localfe.hh>
+
+int main(int argc, char** argv)
+{
+  bool success = true;
+
+  Dune::CrouzeixRaviartLocalFiniteElement<double,double,1> crozeixRaviart1dLFE;
+  TEST_FE3(crozeixRaviart1dLFE, DisableNone, 2 /* difforder */);
+
+  Dune::CrouzeixRaviartLocalFiniteElement<double,double,2> crozeixRaviart2dLFE;
+  TEST_FE3(crozeixRaviart2dLFE, DisableNone, 2 /* difforder */);
+
+  Dune::CrouzeixRaviartLocalFiniteElement<double,double,3> crozeixRaviart3dLFE;
+  TEST_FE3(crozeixRaviart3dLFE, DisableNone, 2 /* difforder */);
+
+  Dune::CrouzeixRaviartLocalFiniteElement<double,double,4> crozeixRaviart4dLFE;
+  TEST_FE3(crozeixRaviart4dLFE, DisableNone, 2 /* difforder */);
+
+  return success ? 0 : 1;
+}

a238f676f5084646c58ec1311e19428f5dfa41d3
Oliver Sander <oliver.sander@tu-dresden.de>
2019-08-18 16:59:46 +0200

    New class LagrangeCubeLocalFiniteElement
    This new class is intended to unify the existing implementations for
    Lagrange elements on cube domains with compile-time dimension and
    order.  This will make the API simpler (less implementations classes),
    and the code gets shorter, too.
    
    This commit also replaces the existing Q1LocalFiniteElement
    implementation by the new one based on LagrangeCubeLocalFiniteElement.
    This happens in a completely backward-compatible manner.

--- dune/localfunctions/lagrange/q1.hh
+++ dune/localfunctions/lagrange/q1.hh
@@ -8,9 +8,7 @@
 
 #include <dune/localfunctions/common/localfiniteelementtraits.hh>
 #include <dune/localfunctions/common/localtoglobaladaptors.hh>
-#include <dune/localfunctions/lagrange/q1/q1localbasis.hh>
-#include <dune/localfunctions/lagrange/q1/q1localcoefficients.hh>
-#include <dune/localfunctions/lagrange/q1/q1localinterpolation.hh>
+#include <dune/localfunctions/lagrange/lagrangecube.hh>
 
 namespace Dune
 {
@@ -19,60 +17,11 @@ namespace Dune
       \tparam D Domain data type
       \tparam R Range data type
       \tparam dim Dimension of the cube
+
+      \deprecated This class is deprecated!  Please use LagrangeCubeLocalFiniteElement instead.
    */
   template<class D, class R, int dim>
-  class Q1LocalFiniteElement
-  {
-  public:
-    // user-defined default constructor is required for clang 3.8,
-    // see https://gitlab.dune-project.org/core/dune-localfunctions/merge_requests/60
-    /** default constructor */
-    Q1LocalFiniteElement() {}
-
-    /** \todo Please doc me !
-     */
-    typedef LocalFiniteElementTraits<Q1LocalBasis<D,R,dim>,Q1LocalCoefficients<dim>,
-        Q1LocalInterpolation<dim,Q1LocalBasis<D,R,dim> > > Traits;
-
-    /** \todo Please doc me !
-     */
-    const typename Traits::LocalBasisType& localBasis () const
-    {
-      return basis;
-    }
-
-    /** \todo Please doc me !
-     */
-    const typename Traits::LocalCoefficientsType& localCoefficients () const
-    {
-      return coefficients;
-    }
-
-    /** \todo Please doc me !
-     */
-    const typename Traits::LocalInterpolationType& localInterpolation () const
-    {
-      return interpolation;
-    }
-
-    /** \brief Number of shape functions in this finite element */
-    unsigned int size () const
-    {
-      return basis.size();
-    }
-
-    /** \todo Please doc me !
-     */
-    static constexpr GeometryType type ()
-    {
-      return GeometryTypes::cube(dim);
-    }
-
-  private:
-    Q1LocalBasis<D,R,dim> basis;
-    Q1LocalCoefficients<dim> coefficients;
-    Q1LocalInterpolation<dim,Q1LocalBasis<D,R,dim> > interpolation;
-  };
+  using Q1LocalFiniteElement = LagrangeCubeLocalFiniteElement<D,R,dim,1>;
 
   //! Factory for global-valued Q1 elements
   /**
@@ -83,14 +32,14 @@ namespace Dune
   template<class Geometry, class RF>
   class Q1FiniteElementFactory :
     public ScalarLocalToGlobalFiniteElementAdaptorFactory<
-        Q1LocalFiniteElement<
-            typename Geometry::ctype, RF, Geometry::mydimension
+        LagrangeCubeLocalFiniteElement<
+            typename Geometry::ctype, RF, Geometry::mydimension, 1
             >,
         Geometry
         >
   {
-    typedef Q1LocalFiniteElement<
-        typename Geometry::ctype, RF, Geometry::mydimension
+    typedef LagrangeCubeLocalFiniteElement<
+        typename Geometry::ctype, RF, Geometry::mydimension, 1
         > LFE;
     typedef ScalarLocalToGlobalFiniteElementAdaptorFactory<LFE, Geometry> Base;
 

75da6412cfc6a750d53d652133ee8033a69c13b1
Oliver Sander <oliver.sander@tu-dresden.de>
2019-08-21 16:23:20 +0200

    Add an explicit empty user-defined default constructor
    For some reason clang-3.8 requires this.

--- dune/localfunctions/lagrange/lagrangecube.hh
+++ dune/localfunctions/lagrange/lagrangecube.hh
@@ -714,6 +714,13 @@ namespace Dune
                                             Impl::LagrangeCubeLocalCoefficients<d,k>,
                                             Impl::LagrangeCubeLocalInterpolation<Impl::LagrangeCubeLocalBasis<D,R,d,k> > >;
 
+    /** \brief Default constructor
+     *
+     * \deprecated This explicit implementation only exists to work around a bug in clang 3.8
+     *   which disappeared in clang 6
+     */
+    LagrangeCubeLocalFiniteElement() {}
+
     /** \brief Returns the local basis, i.e., the set of shape functions
      */
     const typename Traits::LocalBasisType& localBasis () const

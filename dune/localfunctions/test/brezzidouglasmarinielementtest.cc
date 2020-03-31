4e049da0a38d4b8dd48467ed4a2bad51e0683c6f
Oliver Sander <oliver.sander@tu-dresden.de>
2020-03-31 22:19:57 +0200

    Add test whether shape functions can represent constants
    This patch adds a new test that checks whether the functions space
    spanned by the shape functions includes the constant functions.
    This holds for all finite elements we currently implement.
    
    This test can be disabled by setting DisableRepresentConstants.
    
    The patch does disable the new test for the second order
    Lagrange finite elements on the pyramid element, because
    that one is apparently buggy.  A fix will come in a separate commit.

--- dune/localfunctions/test/brezzidouglasmarinielementtest.cc
+++ dune/localfunctions/test/brezzidouglasmarinielementtest.cc
@@ -17,7 +17,9 @@ int main(int argc, char** argv)
   TEST_FE(bdm1cube2dlfem);
 
   Dune::BrezziDouglasMariniCubeLocalFiniteElement<double,double,3,1> bdm1cube3dlfem(1);
-  TEST_FE2(bdm1cube3dlfem, DisableLocalInterpolation);
+  // \todo Implement the missing LocalInterpolation
+  // DisableRepresentConstants is only set because the test also uses DisableLocalInterpolation internally.
+  TEST_FE2(bdm1cube3dlfem, DisableLocalInterpolation + DisableRepresentConstants);
 
   Dune::BrezziDouglasMariniCubeLocalFiniteElement<double,double,2,2> bdm2cube2dlfem(1);
   TEST_FE(bdm2cube2dlfem);

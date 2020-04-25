99866dd9995649458bec93e53ee4ee3f01c3874e
Simon Praetorius <simon.praetorius@tu-dresden.de>
2020-04-25 22:15:37 +0200

    Pass Domain and Range type to PolynomialBasis

--- dune/localfunctions/utility/polynomialbasis.hh
+++ dune/localfunctions/utility/polynomialbasis.hh
@@ -63,7 +63,7 @@ namespace Dune
   template< class Eval, class CM, class D=double, class R=double >
   class PolynomialBasis
   {
-    typedef PolynomialBasis< Eval, CM > This;
+    typedef PolynomialBasis This;
     typedef Eval Evaluator;
 
   public:
@@ -352,8 +352,8 @@ namespace Dune
   private:
     typedef Eval Evaluator;
 
-    typedef PolynomialBasisWithMatrix< Evaluator, CM > This;
-    typedef PolynomialBasis<Evaluator, CM, D, R> Base;
+    typedef PolynomialBasisWithMatrix This;
+    typedef PolynomialBasis<Eval,CM,D,R> Base;
 
   public:
     typedef typename Base::Basis Basis;

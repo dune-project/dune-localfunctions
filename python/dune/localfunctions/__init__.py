5d6d3dd6fedb01f1ede747b9480542c28ca21925
Ansgar Burchardt <ansgar.burchardt@tu-dresden.de>
2022-09-22 16:03:54 +0200

    add SPDX information
    See https://reuse.software/ for a description.

--- python/dune/localfunctions/__init__.py
+++ python/dune/localfunctions/__init__.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: Copyright (C) DUNE Project contributors, see file LICENSE.md in module root
+# SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
+
 from __future__ import absolute_import, unicode_literals
 
 from ._localfunctions import *

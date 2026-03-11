b4b09734280943388800d74477e122764f65cd55
Simon Praetorius <simon.praetorius@tu-dresden.de>
2026-03-11 16:46:36 +0100

    Test the dynamic lagrange functions

--- dune/localfunctions/test/lagrangeshapefunctiontest.cc
+++ dune/localfunctions/test/lagrangeshapefunctiontest.cc
@@ -117,9 +117,13 @@ int main (int argc, char *argv[])
   {
     Dune::Hybrid::forEach(std::index_sequence<1,2,3,4>{},[&](auto order)
     {
-      auto lfe = LagrangeSimplexLocalFiniteElement<double,double,dim,order>();
-      testSuite.check(testPk(lfe))
-        << "Lagrange property not satisfied for " << Dune::className(lfe);
+      auto lfe_static = LagrangeSimplexLocalFiniteElement<double,double,dim,order>();
+      testSuite.check(testPk(lfe_static))
+        << "Lagrange property not satisfied for " << Dune::className(lfe_static);
+
+      auto lfe_dynamic = LagrangeSimplexLocalFiniteElement<double,double,dim>(order);
+      testSuite.check(testPk(lfe_dynamic))
+        << "Lagrange property not satisfied for " << Dune::className(lfe_dynamic);
     });
   });
 
@@ -179,8 +183,11 @@ DUNE_NO_DEPRECATED_END
     Dune::Hybrid::forEach(std::make_index_sequence<5>{},[&](auto order)
     {
       auto diffOrder = 2;
-      auto lfe = LagrangeSimplexLocalFiniteElement<double,double,dim,order>();
-      testSuite.subTest(testVirtualLFE(lfe, DisableNone, diffOrder));
+      auto lfe_static = LagrangeSimplexLocalFiniteElement<double,double,dim,order>();
+      testSuite.subTest(testVirtualLFE(lfe_static, DisableNone, diffOrder));
+
+      auto lfe_dynamic = LagrangeSimplexLocalFiniteElement<double,double,dim>(order);
+      testSuite.subTest(testVirtualLFE(lfe_dynamic, DisableNone, diffOrder));
     });
   });
 

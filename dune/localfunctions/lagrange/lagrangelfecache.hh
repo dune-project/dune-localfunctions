2af4b8a23805aea9072102ba47b88d0a5bc68945
Carsten Gräser <graeser@dune-project.org>
2026-05-05 13:35:07 +0200

    Add support for run-time order in LagrangeLocalFiniteElementCache
    Since `LagrangeLocalFiniteElementCache` basically provides type-erasure
    wrt the `GeometryType` for the `Lagrange[GT]LocalFiniteElement`
    implementations, it should also support the dynamic order mode
    of the latter. This is implemented with the current patch.

--- dune/localfunctions/lagrange/lagrangelfecache.hh
+++ dune/localfunctions/lagrange/lagrangelfecache.hh
@@ -27,75 +27,68 @@ namespace Impl {
 
   // Provide implemented Lagrange local finite elements
 
-  template<class D, class R, std::size_t dim, std::size_t order>
-  struct ImplementedLagrangeFiniteElements : public FixedDimLocalGeometryTypeIndex<dim>
+  template<class D, class R, std::size_t dim, int compileTimeOrder>
+  class ImplementedLagrangeFiniteElements : public FixedDimLocalGeometryTypeIndex<dim>
   {
+    int runTimeOrder_;
+  public:
+
+    ImplementedLagrangeFiniteElements(int runTimeOrder = compileTimeOrder)
+      : runTimeOrder_(runTimeOrder)
+    {
+      if (runTimeOrder < 0)
+        DUNE_THROW(Dune::InvalidStateException, "LagrangeLocalFiniteElementCache: Run-time order must be non-negative.");
+      if ((compileTimeOrder >= 0) and (runTimeOrder != compileTimeOrder))
+        DUNE_THROW(Dune::InvalidStateException, "LagrangeLocalFiniteElementCache: Run-time order must be consistent with compile-time order when providing both.");
+    }
+
     using FixedDimLocalGeometryTypeIndex<dim>::index;
-    static auto getImplementations()
-    {
-      return std::make_tuple(
-        std::make_pair(index(GeometryTypes::simplex(dim)), []() { return LagrangeSimplexLocalFiniteElement<D,R,dim,order>(); }),
-        std::make_pair(index(GeometryTypes::cube(dim)),    []() { return LagrangeCubeLocalFiniteElement<D,R,dim,order>(); })
-      );
-    }
-  };
 
-  template<class D, class R, std::size_t dim>
-  struct ImplementedLagrangeFiniteElements<D,R,dim,0> : public FixedDimLocalGeometryTypeIndex<dim>
-  {
-    using FixedDimLocalGeometryTypeIndex<dim>::index;
-    static auto getImplementations()
+    auto getImplementations() const
     {
-      return std::make_tuple(
-        std::make_pair(index(GeometryTypes::simplex(dim)), []() { return P0LocalFiniteElement<D,R,dim>(GeometryTypes::simplex(dim)); }),
-        std::make_pair(index(GeometryTypes::cube(dim)),    []() { return P0LocalFiniteElement<D,R,dim>(GeometryTypes::cube(dim)); }),
-        std::make_pair(index(GeometryTypes::none(dim)),    []() { return P0LocalFiniteElement<D,R,dim>(GeometryTypes::none(dim)); })
-      );
-    }
-  };
-
-  template<class D, class R>
-  struct ImplementedLagrangeFiniteElements<D,R,3,0> : public FixedDimLocalGeometryTypeIndex<3>
-  {
-    using FixedDimLocalGeometryTypeIndex<3>::index;
-    static auto getImplementations()
-    {
-      return std::make_tuple(
-        std::make_pair(index(GeometryTypes::tetrahedron), []() { return P0LocalFiniteElement<D,R,3>(GeometryTypes::tetrahedron); }),
-        std::make_pair(index(GeometryTypes::hexahedron),  []() { return P0LocalFiniteElement<D,R,3>(GeometryTypes::hexahedron); }),
-        std::make_pair(index(GeometryTypes::prism),       []() { return P0LocalFiniteElement<D,R,3>(GeometryTypes::prism); }),
-        std::make_pair(index(GeometryTypes::pyramid),     []() { return P0LocalFiniteElement<D,R,3>(GeometryTypes::pyramid); })
-      );
-    }
-  };
-
-  template<class D, class R>
-  struct ImplementedLagrangeFiniteElements<D,R,3,1> : public FixedDimLocalGeometryTypeIndex<3>
-  {
-    using FixedDimLocalGeometryTypeIndex<3>::index;
-    static auto getImplementations()
-    {
-      return std::make_tuple(
-        std::make_pair(index(GeometryTypes::tetrahedron), []() { return LagrangeSimplexLocalFiniteElement<D,R,3,1>(); }),
-        std::make_pair(index(GeometryTypes::hexahedron),  []() { return LagrangeCubeLocalFiniteElement<D,R,3,1>(); }),
-        std::make_pair(index(GeometryTypes::prism),       []() { return LagrangePrismLocalFiniteElement<D,R,1>(); }),
-        std::make_pair(index(GeometryTypes::pyramid),     []() { return LagrangePyramidLocalFiniteElement<D,R,1>(); })
-      );
-    }
-  };
-
-  template<class D, class R>
-  struct ImplementedLagrangeFiniteElements<D,R,3,2> : public FixedDimLocalGeometryTypeIndex<3>
-  {
-    using FixedDimLocalGeometryTypeIndex<3>::index;
-    static auto getImplementations()
-    {
-      return std::make_tuple(
-        std::make_pair(index(GeometryTypes::tetrahedron), []() { return LagrangeSimplexLocalFiniteElement<D,R,3,2>(); }),
-        std::make_pair(index(GeometryTypes::hexahedron),  []() { return LagrangeCubeLocalFiniteElement<D,R,3,2>(); }),
-        std::make_pair(index(GeometryTypes::prism),       []() { return LagrangePrismLocalFiniteElement<D,R,2>(); }),
-        std::make_pair(index(GeometryTypes::pyramid),     []() { return LagrangePyramidLocalFiniteElement<D,R,2>(); })
-      );
+      if constexpr (compileTimeOrder >= 0)
+      {
+        if constexpr ((dim != 3) and (compileTimeOrder == 0))
+          return std::make_tuple(
+            std::make_pair(index(GeometryTypes::simplex(dim)), []() { return P0LocalFiniteElement<D,R,dim>(GeometryTypes::simplex(dim)); }),
+            std::make_pair(index(GeometryTypes::cube(dim)),    []() { return P0LocalFiniteElement<D,R,dim>(GeometryTypes::cube(dim)); }),
+            std::make_pair(index(GeometryTypes::none(dim)),    []() { return P0LocalFiniteElement<D,R,dim>(GeometryTypes::none(dim)); })
+          );
+        else if constexpr ((dim == 3) and (compileTimeOrder <= 2))
+          return std::make_tuple(
+            std::make_pair(index(GeometryTypes::tetrahedron), []() { return LagrangeSimplexLocalFiniteElement<D,R,dim,compileTimeOrder>(); }),
+            std::make_pair(index(GeometryTypes::hexahedron),  []() { return LagrangeCubeLocalFiniteElement<D,R,dim,compileTimeOrder>(); }),
+            std::make_pair(index(GeometryTypes::prism),       []() { return LagrangePrismLocalFiniteElement<D,R,compileTimeOrder>(); }),
+            std::make_pair(index(GeometryTypes::pyramid),     []() { return LagrangePyramidLocalFiniteElement<D,R,compileTimeOrder>(); })
+          );
+        else
+          return std::make_tuple(
+            std::make_pair(index(GeometryTypes::simplex(dim)), []() { return LagrangeSimplexLocalFiniteElement<D,R,dim,compileTimeOrder>(); }),
+            std::make_pair(index(GeometryTypes::cube(dim)),    []() { return LagrangeCubeLocalFiniteElement<D,R,dim,compileTimeOrder>(); })
+          );
+      }
+      else
+      {
+        if constexpr (dim == 3)
+        {
+          constexpr auto unusedIndex = std::numeric_limits<decltype(index(GeometryTypes::simplex(dim)))>::max();
+          auto prismOrder = (runTimeOrder_<= 2) ? runTimeOrder_ : 0;
+          auto prismIndex = (runTimeOrder_<= 2) ? index(GeometryTypes::prism) : unusedIndex;
+          auto pyramidOrder = (runTimeOrder_<= 2) ? runTimeOrder_ : 0;
+          auto pyramidIndex = (runTimeOrder_<= 2) ? index(GeometryTypes::pyramid) : unusedIndex;
+          return std::make_tuple(
+            std::make_pair(index(GeometryTypes::tetrahedron), [&]() { return LagrangeSimplexLocalFiniteElement<D,R,dim,compileTimeOrder>(runTimeOrder_); }),
+            std::make_pair(index(GeometryTypes::hexahedron),  [&]() { return LagrangeCubeLocalFiniteElement<D,R,dim,compileTimeOrder>(runTimeOrder_); }),
+            std::make_pair(prismIndex,                        [=]() { return LagrangePrismLocalFiniteElement<D,R,compileTimeOrder>(prismOrder); }),
+            std::make_pair(pyramidIndex,                      [=]() { return LagrangePyramidLocalFiniteElement<D,R,compileTimeOrder>(pyramidOrder); })
+          );
+        }
+        else
+          return std::make_tuple(
+            std::make_pair(index(GeometryTypes::simplex(dim)), [&]() { return LagrangeSimplexLocalFiniteElement<D,R,dim,compileTimeOrder>(runTimeOrder_); }),
+            std::make_pair(index(GeometryTypes::cube(dim)),    [&]() { return LagrangeCubeLocalFiniteElement<D,R,dim,compileTimeOrder>(runTimeOrder_); })
+          );
+      }
     }
   };
 
@@ -110,12 +103,14 @@ namespace Impl {
  * \tparam D Type used for domain coordinates
  * \tparam R Type used for shape function values
  * \tparam dim Element dimension
- * \tparam order Element order
+ * \tparam compileTimeOrder Polynomial order of the shape functions (or -1 for run-time order)
  *
  * The cached finite element implementations can be obtained using get(GeometryType).
+ * If the \ref compileTimeOrder provided as template parameter is `-1`, the order
+ * has to be provided as constructor argument.
  */
-template<class D, class R, std::size_t dim, std::size_t order>
-using LagrangeLocalFiniteElementCache = LocalFiniteElementVariantCache<Impl::ImplementedLagrangeFiniteElements<D,R,dim,order>>;
+template<class D, class R, std::size_t dim, int compileTimeOrder = -1>
+using LagrangeLocalFiniteElementCache = LocalFiniteElementVariantCache<Impl::ImplementedLagrangeFiniteElements<D,R,dim, compileTimeOrder>>;
 
 
 
